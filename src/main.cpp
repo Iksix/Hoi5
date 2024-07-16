c14ecacba10764b5e3732cce16edc4b9d3000d8c
iksixx <pocetikyt@gmail.com>
2024-07-16 19:39:40 +0700

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,69 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+};
+
+void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+    }
+};
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
     if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
     {
-        glfwTerminate();
+        cout << "glfwInit failed" << endl;
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Hoi5", nullptr, nullptr);
+    if (!pWindow)
     {
-        /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        cout << "glfwCreateWindow failed" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    //Callbacks
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallback);
+    glfwSetKeyCallback(pWindow, glfwKeyCallback);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+    glfwMakeContextCurrent(pWindow);
 
-        /* Poll for and process events */
+    if (!gladLoadGL())
+    {
+        cout << "Can't load GLAD!" << endl;
+        return -1;
+    }
+    
+    cout << "Renderer: " << glGetString(GL_RENDERER) << endl;
+    cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
+
+    glClearColor(1, 1, 0, 1);
+
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(pWindow);
+
         glfwPollEvents();
     }
 
