86f22529761e7de480f7a22038892102f85259d2
iksixx <pocetikyt@gmail.com>
2024-07-16 18:40:21 +0700

    some

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char* argv[])
 {
     std::cout << "Hello, world" << std::endl;
+    std::cout << "Hello, world 1" << std::endl;
     return 0;
 }
